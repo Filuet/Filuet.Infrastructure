8ba8167c4ed840d93944ee40a89cd1fbf7944d45
Igor <igor.gutsaev@filuet.ru>
2021-10-29 14:43:07 +0300

    +email address validator

--- Abstractions/Helpers/StringHelpers.cs
+++ Abstractions/Helpers/StringHelpers.cs
@@ -14,5 +14,12 @@ namespace Filuet.Infrastructure.Abstractions.Helpers
             Regex regex = new Regex(@"^[{(]?[0-9A-F]{8}[-]?([0-9A-F]{4}[-]?){3}[0-9A-F]{12}[)}]?$", RegexOptions.IgnoreCase);
             return regex.Match(source).Success;
         }
+
+        public static bool IsEmail(this string input)
+        {
+            Regex regex = new Regex(@"^([\w\.\-]+)@([\w\-]+)((\.(\w){2,3})+)$");
+            Match match = regex.Match(input);
+            return match.Success;
+        }
     }
 }
