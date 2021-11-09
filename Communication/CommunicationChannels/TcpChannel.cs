c26c43aba2d4a4ab500ef10d8096c0c515dfdc73
Igor Gutsaev <igor.gutsaev@filuet.ru>
2021-11-09 14:22:19 +0300

    Tcp Communication improvements

--- Communication/CommunicationChannels/TcpChannel.cs
+++ Communication/CommunicationChannels/TcpChannel.cs
@@ -1,34 +1,42 @@
 ﻿using System.Linq;
 using System.Net;
 using System.Net.Sockets;
+using System.Threading;
 
 namespace Filuet.Infrastructure.Communication
 {
     public class TcpChannel : ICommunicationChannel
     {
-        private string _ip;
-        private int _port;
-
-        public TcpChannel(string ip, int port = 5000)
+        public TcpChannel(string ip, ushort port = 5000, uint responseDelayMs = 100)
         {
             _ip = ip;
             _port = port;
+            _responseDelayMs = responseDelayMs;
         }
 
         public byte[] SendCommand(byte[] data)
         {
-            TcpClient client = new TcpClient();
-            client.Connect(new IPEndPoint(IPAddress.Parse(_ip), _port));
+            lock (_mutex)
+            {
+                TcpClient client = new TcpClient();
+                client.Connect(new IPEndPoint(IPAddress.Parse(_ip), (int)_port));
 
-            NetworkStream stream = client.GetStream();
+                NetworkStream stream = client.GetStream();
 
-            stream.Write(data, 0, data.Length);
-            data = new byte[32];
-            int bytes = stream.Read(data, 0, data.Length);
+                stream.Write(data, 0, data.Length);
+                data = new byte[32];
+                Thread.Sleep((int)_responseDelayMs);
+                int bytes = stream.Read(data, 0, data.Length);
 
-            stream.Close();
-            client.Close();
-            return data.Take(bytes).ToArray();
+                stream.Close();
+                client.Close();
+                return data.Take(bytes).ToArray();
+            }
         }
+
+        private readonly string _ip;
+        private readonly ushort _port;
+        private readonly uint _responseDelayMs;
+        private object _mutex;
     }
 }
