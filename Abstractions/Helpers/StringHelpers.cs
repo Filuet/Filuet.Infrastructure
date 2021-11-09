211c2b42b06f2ce6219a0be6d4870abc82464e69
Igor Gutsaev <igor.gutsaev@filuet.ru>
2021-11-09 13:26:11 +0300

    + some string helpers

--- Abstractions/Helpers/StringHelpers.cs
+++ Abstractions/Helpers/StringHelpers.cs
@@ -4,6 +4,9 @@ namespace Filuet.Infrastructure.Abstractions.Helpers
 {
     public static class StringHelpers
     {
+        public static bool IsMacAddress(this string macAddress)
+            => CheckMatch(macAddress, "^([0-9A-Fa-f]{2}[:-]){5}([0-9A-Fa-f]{2})$");
+
         public static bool IsGuid(this string source)
         {
             source = source.Trim().Replace(" ", "");
@@ -11,15 +14,34 @@ namespace Filuet.Infrastructure.Abstractions.Helpers
             if (string.IsNullOrWhiteSpace(source) || source.Length < 32)
                 return false;
 
-            Regex regex = new Regex(@"^[{(]?[0-9A-F]{8}[-]?([0-9A-F]{4}[-]?){3}[0-9A-F]{12}[)}]?$", RegexOptions.IgnoreCase);
-            return regex.Match(source).Success;
+            return CheckMatch(source, @"^[{(]?[0-9A-F]{8}[-]?([0-9A-F]{4}[-]?){3}[0-9A-F]{12}[)}]?$");
         }
 
         public static bool IsEmail(this string input)
+            => CheckMatch(input, @"^([\w\.\-]+)@([\w\-]+)((\.(\w){2,3})+)$");
+
+        public static bool IsPhone(this string mobile)
+            => CheckMatch(mobile, @"^(\+\d{1,2}\s)\(?\d{3}\)?[\s.-]\d{3}[\s.-]\d{4}$");
+
+        public static bool IsValidJson(string json)
         {
-            Regex regex = new Regex(@"^([\w\.\-]+)@([\w\-]+)((\.(\w){2,3})+)$");
-            Match match = regex.Match(input);
-            return match.Success;
+            try
+            {
+                System.Text.Json.JsonDocument.Parse(json);
+                return true;
+            }
+            catch { }
+
+            return false;
+        }
+
+        private static bool CheckMatch(string source, string regularExpression)
+        {
+            if (string.IsNullOrWhiteSpace(source) || string.IsNullOrWhiteSpace(regularExpression))
+                return false;
+
+            Regex regex = new Regex(regularExpression, RegexOptions.IgnoreCase);
+            return regex.Match(source).Success;
         }
     }
 }
