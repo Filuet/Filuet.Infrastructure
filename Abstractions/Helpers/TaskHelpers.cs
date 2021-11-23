0c15fa8b837241d703b2068073af26922e707afd
Igor Gutsaev <igor.gutsaev@filuet.ru>
2021-11-23 15:22:50 +0300

    + task helpers

---
+++ Abstractions/Helpers/TaskHelpers.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace Filuet.Infrastructure.Abstractions.Helpers
+{
+    public static class TaskHelpers
+    {
+        public static bool ExecuteWithTimeLimit(TimeSpan timeSpan, Action action)
+        {
+            try
+            {
+                Task task = Task.Factory.StartNew(() => action());
+                task.Wait(timeSpan);
+                return task.IsCompleted;
+            }
+            catch (AggregateException ae)
+            {
+                throw ae.InnerExceptions[0];
+            }
+        }
+    }
+}
