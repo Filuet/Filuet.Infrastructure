d933799ebc4142f136b200c280977d3d88c534d8
Igor <igor.gutsaev@filuet.ru>
2022-01-27 14:17:05 +0300

    bugfix: orderDto can contain no uncollected items or null

--- domains/Ordering/OrderHelpers.cs
+++ domains/Ordering/OrderHelpers.cs
@@ -25,7 +25,7 @@ namespace Filuet.Infrastructure.Ordering.Helpers
                 Obtaining = order.Obtaining.GetCode(),
                 Amount = new MoneyDto { Value = order.Total.Value, Currency = order.Total.Currency.GetCode() },
                 Paid = new MoneyDto { Value = order.Paid.Value, Currency = order.Paid.Currency.GetCode() },
-                Items = order.Items.Select(x => new OrderLineDto
+                Items = order.Items?.Select(x => new OrderLineDto
                 {
                     ProductUID = x.ProductUID,
                     Name = x.Name,
@@ -33,7 +33,7 @@ namespace Filuet.Infrastructure.Ordering.Helpers
                     Quantity = x.Quantity,
                     Amount = new MoneyDto { Value = x.Amount.Value, Currency = x.Amount.Currency.GetCode() }
                 }),
-                UncollectedItems = order.UncollectedItems.Select(x => new OrderItemDto {
+                UncollectedItems = order.UncollectedItems?.Select(x => new OrderItemDto {
                     ProductUID = x.ProductUID,
                     Quantity = x.Quantity 
                 })
