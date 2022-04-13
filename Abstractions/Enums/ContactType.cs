55d6f608d9736143c5583c6815b66a1f4cf5aa3a
Igor <igor.gutsaev@filuet.ru>
2022-04-13 13:58:30 +0300

    add codes to contact types

--- Abstractions/Enums/ContactType.cs
+++ Abstractions/Enums/ContactType.cs
@@ -1,8 +1,12 @@
-﻿namespace Filuet.Infrastructure.Abstractions.Enums
+﻿using Filuet.Infrastructure.Attributes;
+
+namespace Filuet.Infrastructure.Abstractions.Enums
 {
     public enum ContactType
     {
+        [Code("EMAIL")]
         Email = 0x01,
+        [Code("PHONE")]
         Prone
     }
 }
