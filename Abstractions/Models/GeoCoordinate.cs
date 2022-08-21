ac38ff912e15c850353fa674543b70c6b5efd518
Igor Gutsaev <igor.gutsaev@filuet.com>
2022-08-21 12:39:13 +0300

    add GeoCordinate

---
+++ Abstractions/Models/GeoCoordinate.cs
@@ -0,0 +1,24 @@
+﻿using System.Text.Json.Serialization;
+
+namespace Filuet.Infrastructure.Abstractions.Models
+{
+    public struct GeoCoordinate
+    {
+        private readonly double latitude;
+        private readonly double longitude;
+
+        [JsonPropertyName("lat")]
+        public double Latitude => latitude;
+
+        [JsonPropertyName("lon")]
+        public double Longitude => longitude;
+
+        public GeoCoordinate(double latitude, double longitude)
+        {
+            this.latitude = latitude;
+            this.longitude = longitude;
+        }
+
+        public override string ToString() => string.Format("{0},{1}", Latitude, Longitude);
+    }
+}
