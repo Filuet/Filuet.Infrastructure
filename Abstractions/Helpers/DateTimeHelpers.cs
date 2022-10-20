686dae512d0f660f2b2d08a78e5adb39364c2d62
Igor Gutsaev <igor.gutsaev@filuet.ru>
2022-10-20 13:12:19 +0300

    with dite time helper

---
+++ Abstractions/Helpers/DateTimeHelpers.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Filuet.Infrastructure.Abstractions.Helpers
+{
+    public static class DateTimeHelpers
+    {
+        public static long ToUnixTimestamp(this DateTime dt)
+        {
+            long unixTimestamp = (long)(dt.Subtract(new DateTime(1970, 1, 1))).TotalSeconds;
+            return unixTimestamp;
+        }
+
+        public static DateTime FromUnixTimestamp(this long ut)
+        {
+            DateTime start = new DateTime(1970, 1, 1, 0, 0, 0, 0, DateTimeKind.Utc);
+            start = start.AddSeconds(ut);
+            return start;
+        }
+    }
+}
