618f241bcd3a43c0aece26ad2e7599fd047d19c4
Igor Gutsaev <igor.gutsaev@filuet.com>
2022-11-01 12:46:34 +0300

    with interval

---
+++ Abstractions/Enums/Interval.cs
@@ -0,0 +1,16 @@
+﻿using Filuet.Infrastructure.Attributes;
+
+namespace Filuet.Infrastructure.Abstractions.Enums
+{
+    public enum Interval
+    {
+        [Code("day")]
+        Day = 0x01,
+        [Code("week")]
+        week,
+        [Code("month")]
+        Month,
+        [Code("year")]
+        Year
+    }
+}
