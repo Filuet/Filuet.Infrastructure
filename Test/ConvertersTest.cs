e08e76d33d31ac986a47297885c41ef52e9e6a7f
Igor Gutsaev <igor.gutsaev@filuet.com>
2022-12-02 13:47:09 +0300

    add Enum safe converter

--- Test/ConvertersTest.cs
+++ Test/ConvertersTest.cs
@@ -1,4 +1,6 @@
 ﻿using Filuet.Infrastructure.Abstractions.Converters;
+using Filuet.Infrastructure.Abstractions.Enums;
+using Filuet.Infrastructure.Abstractions.Helpers;
 using System.Text.Json;
 using Xunit;
 
@@ -13,6 +15,24 @@ namespace Test
             options.Converters.Add(new N2JsonConverter());
 
             string d = JsonSerializer.Serialize(new { a = 4.012m }, options);
+        }
+
+        [Theory]
+        [InlineData("RR")]
+        public void Test_EnumHelpers_Try_GetCountry_By_Invalid_code(string code)
+        {
+            // Prepare
+            Country? expected = 0;
+
+            // Pre-validate
+            Assert.True(code.Length == 2);
+
+            // Perform
+            bool result = EnumHelpers.TryGetValueFromCode(code, out Country location);
+
+            // Validate
+            Assert.False(result);
+            Assert.Equal(expected, location);
         }
     }
 }
