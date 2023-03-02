a74730f92288c9e25dd1b1d89bcbfdc57787f410
Igor Gutsaev <igor.gutsaev@filuet.com>
2023-03-02 16:28:08 +0300

    add AscKioskMode  description attribute

--- Abstractions/Enums/AscKioskMode.cs
+++ Abstractions/Enums/AscKioskMode.cs
@@ -1,14 +1,18 @@
 ﻿using Filuet.Infrastructure.Attributes;
+using System.ComponentModel;
 
 namespace Filuet.Infrastructure.Abstractions.Enums
 {
     public enum AscKioskMode : int
     {
         [Code("AA")]
+        [Description("AUTOATTENDANT")]
         AA = 0x01,
         [Code("AS")]
+        [Description("AUTOSTORE")]
         AS = 0x02,
         [Code("Mixed")]
+        [Description("MIXED")]
         AAAS = 0x03
     }
 }
