a88c0ce2fdaef8d9e7f89ccb339d926111278590
Igor Gutsaev <igor.gutsaev@filuet.com>
2025-01-27 16:07:08 +0300

    AscActivityTypeConverter

---
+++ Abstractions/Converters/AscActivityTypeConverter.cs
@@ -0,0 +1,17 @@
+﻿using Filuet.Infrastructure.Abstractions.Enums;
+using Filuet.Infrastructure.Abstractions.Helpers;
+using System;
+using System.Text.Json.Serialization;
+using System.Text.Json;
+
+namespace Filuet.Infrastructure.Abstractions.Converters
+{
+    public class AscActivityTypeConverter : JsonConverter<AscActivityType>
+    {
+        public override AscActivityType Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
+            => EnumHelpers.GetValueFromCode<AscActivityType>(reader.GetString());
+
+        public override void Write(Utf8JsonWriter writer, AscActivityType activity, JsonSerializerOptions options)
+            => writer.WriteStringValue(EnumHelpers.GetCode(activity));
+    }
+}
