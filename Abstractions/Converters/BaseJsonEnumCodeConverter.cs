c2d3a86bb6e662c701c8f405e1d6a324114671d7
Igor Gutsaev <igor.gutsaev@filuet.com>
2025-03-27 14:45:55 +0300

    with BaseJsonEnumCodeConverter

---
+++ Abstractions/Converters/BaseJsonEnumCodeConverter.cs
@@ -0,0 +1,16 @@
+﻿using Filuet.Infrastructure.Abstractions.Helpers;
+using System;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace Filuet.Infrastructure.Abstractions.Converters
+{
+    public abstract class BaseJsonEnumCodeConverter<T> : JsonConverter<T> where T: Enum
+    {
+        public override T Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
+            => EnumHelpers.GetValueFromCode<T>(reader.GetString());
+
+        public override void Write(Utf8JsonWriter writer, T val, JsonSerializerOptions options)
+            => writer.WriteStringValue(val.GetCode());
+    }
+}
