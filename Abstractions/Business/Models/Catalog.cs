22091039a6d6b1b988b947bde3a677c5241c4f6c
Igor Gutsaev <igor.gutsaev@filuet.com>
2025-09-14 02:55:49 +0300

    wip

--- Abstractions/Business/Models/Catalog.cs
+++ Abstractions/Business/Models/Catalog.cs
@@ -1,6 +1,9 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 using System.Text.Json.Serialization;
+using System.Text.RegularExpressions;
+using static System.Net.Mime.MediaTypeNames;
 
 namespace Filuet.Infrastructure.Abstractions.Business.Models
 {
@@ -18,8 +21,8 @@ namespace Filuet.Infrastructure.Abstractions.Business.Models
             => Categories.SelectMany(x => x.Products).Distinct().Where(x => skus.Any(y => string.Equals(x.Sku, y.Trim(), System.StringComparison.InvariantCultureIgnoreCase)));
 
         [JsonIgnore]
-        public bool IsEmpty 
-            => !Categories.Any(x => x.Products.Any()); 
+        public bool IsEmpty
+            => !Categories.Any(x => x.Products.Any());
     }
 
     public class ProductSkuName
@@ -31,11 +34,38 @@ namespace Filuet.Infrastructure.Abstractions.Business.Models
         public string Name { get; set; }
     }
 
-    public class ProductSkuNames {
+    public class ProductSkuNames
+    {
         [JsonPropertyName("sku")]
         public string Sku { get; set; }
 
         [JsonPropertyName("names")]
         public IEnumerable<string> Names { get; set; }
+
+        [JsonIgnore]
+        public int ItemsPerPack
+        {
+            get
+            {
+                if (Names == null || !Names.Any())
+                    return 1;
+
+                MatchCollection matches = null;
+
+                foreach (var name in Names) {
+                    matches = Regex.Matches(name, "\\d+\\s*(ea|шт|gb)");
+                    foreach (Match match in matches) {
+                        var numberMatches = Regex.Matches(match.Value, "\\d+");
+                        if (numberMatches.Any())
+                            return Convert.ToInt32(numberMatches.First().Value);
+                    }
+                }
+
+                return 1;
+            }
+        }
+
+        public override string ToString()
+            => $"{Sku} {(Names.Any() ? Names.First() : string.Empty)}".Trim();
     }
 }
