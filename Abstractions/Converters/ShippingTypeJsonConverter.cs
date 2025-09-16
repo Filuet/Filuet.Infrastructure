49d5aef2aa8088a31e562da9c0a93037e39e95e8
Igor Gutsaev <igor.gutsaev@filuet.com>
2025-09-16 23:42:06 +0300

    With ShippingTypeJsonConverter

---
+++ Abstractions/Converters/ShippingTypeJsonConverter.cs
@@ -0,0 +1,17 @@
+﻿using Filuet.Infrastructure.Abstractions.Enums;
+using Filuet.Infrastructure.Abstractions.Helpers;
+using System;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace Filuet.Infrastructure.Abstractions.Converters
+{
+    public class ShippingTypeJsonConverter : JsonConverter<ShippingType>
+    {
+        public override ShippingType Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
+            => EnumHelpers.GetValueFromCode<ShippingType>(reader.GetString());
+
+        public override void Write(Utf8JsonWriter writer, ShippingType status, JsonSerializerOptions options)
+            => writer.WriteStringValue(status.GetCode());
+    }
+}
