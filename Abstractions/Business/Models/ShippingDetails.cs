40ee3147319886de2564d8bf6e11e685675b5544
Igor Gutsaev <igor.gutsaev@filuet.com>
2025-09-16 12:35:33 +0300

    with Shipping details

---
+++ Abstractions/Business/Models/ShippingDetails.cs
@@ -0,0 +1,20 @@
+﻿using Filuet.Infrastructure.Abstractions.Enums;
+using Filuet.Infrastructure.Abstractions.Helpers;
+using System.Text.Json.Serialization;
+
+namespace Filuet.Infrastructure.Abstractions.Business.Models
+{
+    public class ShippingDetails
+    {
+        [JsonPropertyName("type")]
+        public ShippingType Type { get; set; } = ShippingType.CourierDelivery;
+        public string StoreCode { get; set; }
+        public string PickUpPointCode { get; set; }
+        public DeliveryDetails Delivery { get; set; }
+
+        public override string ToString()
+            => FluentSwitch.On(Type).Case(ShippingType.CourierDelivery).Then(Delivery.ToString()).
+            Case(ShippingType.Store).Then(StoreCode).
+            Case(ShippingType.PickUpPoint).Then(PickUpPointCode).Default(null);
+    }
+}
---
+++ Abstractions/Enums/ShippingType.cs
@@ -0,0 +1,14 @@
+﻿using Filuet.Infrastructure.Attributes;
+
+namespace Filuet.Infrastructure.Abstractions.Enums
+{
+    public enum ShippingType : byte
+    {
+        [Code("store")]
+        Store = 0x01,
+        [Code("courier")]
+        CourierDelivery = 0x02,
+        [Code("pickup")]
+        PickUpPoint = 0x03
+    }
+}
