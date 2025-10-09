30512535a42017df28d4a042fcb5df68d0a18ed3
Igor Gutsaev <igor.gutsaev@filuet.com>
2025-10-09 11:30:19 +0300

    with CartItem default quantity

--- Abstractions/Business/Models/CartItem.cs
+++ Abstractions/Business/Models/CartItem.cs
@@ -10,7 +10,7 @@ namespace Filuet.Infrastructure.Abstractions.Business.Models
         [JsonPropertyName("qty")]
         public int Quantity { get; set; }
 
-        public static CartItem Create(string sku, int quantity) {
+        public static CartItem Create(string sku, int quantity = 1) {
             if (string.IsNullOrWhiteSpace(sku) || (sku.Trim().Length < 3 && sku.Trim().ToLower() != "it")) // users may use a pronounce to mention the product
                 throw new ArgumentException("Invalid sku");
 
