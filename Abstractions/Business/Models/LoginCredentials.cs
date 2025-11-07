d1dc45641e311f4a65eb47d879428d1320844526
Igor Gutsaev <igor.gutsaev@filuet.com>
2025-11-07 17:06:46 +0300

    with loginCredentials

---
+++ Abstractions/Business/Models/LoginCredentials.cs
@@ -0,0 +1,12 @@
+﻿using System.Text.Json.Serialization;
+
+namespace Filuet.Infrastructure.Abstractions.Business.Models
+{
+    public class LoginCredentials
+    {
+        [JsonPropertyName("username")]
+        public string Username { get; set; }
+        [JsonPropertyName("password")]
+        public string Password { get; set; }
+    }
+}
