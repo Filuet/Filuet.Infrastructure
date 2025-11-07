89acd0ed72af4ba0ea574af0f018abfd7acbaf5b
Igor Gutsaev <igor.gutsaev@filuet.com>
2025-11-07 12:21:26 +0300

    with AscKioskModeConverter

---
+++ Abstractions/Converters/AscKioskModeConverter.cs
@@ -0,0 +1,17 @@
+﻿using Filuet.Infrastructure.Abstractions.Enums;
+using Filuet.Infrastructure.Abstractions.Helpers;
+using System;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace Filuet.Infrastructure.Abstractions.Converters
+{
+    public class AscKioskModeConverter : JsonConverter<AscKioskMode>
+    {
+        public override AscKioskMode Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
+            => EnumHelpers.GetValueFromCode<AscKioskMode>(reader.GetString());
+
+        public override void Write(Utf8JsonWriter writer, AscKioskMode mode, JsonSerializerOptions options)
+            => writer.WriteStringValue(EnumHelpers.GetCode(mode));
+    }
+}
