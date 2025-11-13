5db7034e4225994381e335e6e8160ea8ba60cefc
Igor Gutsaev <igor.gutsaev@filuet.com>
2025-11-13 16:34:03 +0300

    with ChannelJsonConverter

---
+++ Abstractions/Business/Models/AuthenticationToken.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Text.Json.Serialization;
+
+namespace Filuet.Infrastructure.Abstractions.Business.Models
+{
+    public class AuthenticationToken
+    {
+        [JsonPropertyName("token")]
+        public string Token { get; set; }
+        [JsonPropertyName("expiration")]
+        public DateTime Expiration { get; set; }
+    }
+}
---
+++ Abstractions/Converters/ChannelJsonConverter.cs
@@ -0,0 +1,17 @@
+﻿using Filuet.Infrastructure.Abstractions.Enums;
+using Filuet.Infrastructure.Abstractions.Helpers;
+using System;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace Filuet.Infrastructure.Abstractions.Converters
+{
+    public class ChannelJsonConverter : JsonConverter<BotChannel>
+    {
+        public override BotChannel Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
+            => EnumHelpers.GetValueFromCode<BotChannel>(reader.GetString());
+
+        public override void Write(Utf8JsonWriter writer, BotChannel channel, JsonSerializerOptions options)
+            => writer.WriteStringValue(channel.GetCode());
+    }
+}
