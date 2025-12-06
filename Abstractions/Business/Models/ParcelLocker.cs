6721fe4e52d6632e33f311da8eb9263e201ade45
Igor Gutsaev <igor.gutsaev@filuet.com>
2025-12-06 18:40:58 +0300

    with parcel locker model

---
+++ Abstractions/Business/Models/ParcelLocker.cs
@@ -0,0 +1,14 @@
+﻿using Filuet.Infrastructure.Abstractions.Enums;
+
+namespace Filuet.Infrastructure.Abstractions.Business.Models
+{
+    public class ParcelLocker
+    {
+        public string Code { get; set; }
+        public Country Country { get; set; }
+        public string City { get; set; }
+        public string Address { get; set; }
+        public string FreightCode { get; set; }
+        public string WarehouseCode { get; set; }
+    }
+}
