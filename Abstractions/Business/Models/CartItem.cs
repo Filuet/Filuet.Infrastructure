ee9a669b4a178248f394e20938a46f11aa3ff8f3
Igor Gutsaev <igor.gutsaev@filuet.com>
2025-12-25 13:35:37 +0300

    Cart item upd

--- Abstractions/Business/Models/CartItem.cs
+++ Abstractions/Business/Models/CartItem.cs
@@ -9,6 +9,8 @@ namespace Filuet.Infrastructure.Abstractions.Business.Models
         public string Sku { get; set; }
         [JsonPropertyName("qty")]
         public int Quantity { get; set; }
+        [JsonPropertyName("timestamp")]
+        public DateTime Timestamp { get; set; }
 
         public static CartItem Create(string sku, int quantity = 1) {
             if (string.IsNullOrWhiteSpace(sku) || (sku.Trim().Length < 3 && sku.Trim().ToLower() != "it")) // users may use a pronounce to mention the product
@@ -17,7 +19,14 @@ namespace Filuet.Infrastructure.Abstractions.Business.Models
             if (quantity <= 0)
                 throw new ArgumentException("Quantity must be positive");
 
-            return new CartItem { Sku = sku.Trim(), Quantity = quantity };
+            return new CartItem { Sku = sku.Trim(), Quantity = quantity, Timestamp = DateTime.UtcNow };
+        }
+
+        public static CartItem Load(string sku, int quantity, DateTime timestamp) {
+            if (quantity <= 0)
+                throw new ArgumentException("Quantity must be positive");
+
+            return new CartItem { Sku = sku.Trim(), Quantity = quantity, Timestamp = timestamp };
         }
 
         public override string ToString()
