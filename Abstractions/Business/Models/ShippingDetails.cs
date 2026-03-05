ef91483c02dcf4367018d371639ce71d8afab19b
Igor Gutsaev <igor.gutsaev@filuet.com>
2026-03-05 14:21:01 +0300

    filter shipping options

--- Abstractions/Business/Models/ShippingDetails.cs
+++ Abstractions/Business/Models/ShippingDetails.cs
@@ -1,5 +1,6 @@
 ﻿using Filuet.Infrastructure.Abstractions.Enums;
 using Filuet.Infrastructure.Abstractions.Helpers;
+using System.Linq;
 using System.Text.Json.Serialization;
 
 namespace Filuet.Infrastructure.Abstractions.Business.Models
@@ -32,6 +33,36 @@ namespace Filuet.Infrastructure.Abstractions.Business.Models
                 .Case(ShippingType.Locker).Then(Locker.IsSufficient)
                 .Case(ShippingType.PickupPoint).Then(PickupPoint.IsSufficient)
             .Default(false);
+
+        /// <summary>
+        /// Leave only the shipping options that math the available types. Available types vary from country to country and depends on circumstances
+        /// </summary>
+        /// <param name="availableTypes"></param>
+        /// <returns></returns>
+        public ShippingDetails Filter(ShippingType[] availableTypes) {
+            if (availableTypes == null || !availableTypes.Any()) {
+                Store = null;
+                Delivery = null;
+                Locker = null;
+                PickupPoint = null;
+
+                return this;
+            }
+
+            if (!availableTypes.Contains(ShippingType.Store))
+                Store = null;
+
+            if (!availableTypes.Contains(ShippingType.CourierDelivery))
+                Delivery = null;
+
+            if (!availableTypes.Contains(ShippingType.Locker))
+                Locker = null;
+
+            if (!availableTypes.Contains(ShippingType.PickupPoint))
+                PickupPoint = null;
+
+            return this;
+        }
 
         public override string ToString()
             => FluentSwitch.On(Type).Case(ShippingType.CourierDelivery).Then(Delivery.ToString())
